beed7553b38f8ac32168491a8ca46eb469ce24d3
dexterhere04 <sdrtharun@gmail.com>
2026-07-16 08:50:37 +0530

    refactor: moving object hashing to object.go

--- object.go
+++ object.go
@@ -1,7 +1,29 @@
 package main
 
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"os"
+)
+
 type Object struct {
 	Hash string
 	Type string
 	Data []byte
+}
+
+func NewBlob(path string) (*Object, error) {
+	contents, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	header := fmt.Sprintf("blob %d%c", len(contents), 0)
+	data := append([]byte(header), contents...)
+	hash := sha1.Sum(data)
+	return &Object{
+		Hash: hex.EncodeToString(hash[:]),
+		Type: "blob",
+		Data: data,
+	}, nil
 }
